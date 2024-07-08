ebe864d97e3237f147e85a85be6efc1860360ec0
moexu <moexu13@gmail.com>
2024-07-08 15:59:18 -0400

    wip

--- src/Gameboard.jsx
+++ src/Gameboard.jsx
@@ -1,9 +1,17 @@
 const Gameboard = () => {
   return (
-    <div className="bg-slate-800">
-      
+    <div className="bg-slate-800 grid grid-cols-3 grid-rows-3 w-10/12 h-5/6">
+      <div className="space space-1">1</div>
+      <div className="space space-2">2</div>
+      <div className="space space-3">3</div>
+      <div className="space space-4">4</div>
+      <div className="space space-5">5</div>
+      <div className="space space-6">6</div>
+      <div className="space space-7">7</div>
+      <div className="space space-8">8</div>
+      <div className="space space-9">9</div>
     </div>
-  )
-}
+  );
+};
 
 export default Gameboard;
